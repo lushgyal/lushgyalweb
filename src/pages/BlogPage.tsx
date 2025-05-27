a353358ec94be9d2fdb3b1a6df708dd8c14ad937
lushgyal <legacy@lushgaltravel.com>
2025-05-26 21:01:54 -0500

    Update BlogPage.tsx

--- src/pages/BlogPage.tsx
+++ src/pages/BlogPage.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 // Blog post type definition
 type BlogPost = {
-  id: number;
+  id: string;
   title: string;
   excerpt: string;
   content: string;
@@ -17,27 +17,26 @@ type BlogPost = {
 const BlogPage: React.FC = () => {
   const blogPosts: BlogPost[] = [
     {
-      id: 1,
-      title: "5 Ways to Immerse Yourself in Ghanaian Culture",
-      excerpt: "Discover authentic experiences in Ghana...",
-      content: "Full blog post content here...",
-      author: "Alicia",
-      date: "May 15, 2025",
-      category: "Ghana",
-      imageUrl: "/images/ghana.jpg",
-      tags: ["Ghana", "Culture", "Travel"]
-    }
+      id: '1',
+      title: '5 Ways to Immerse Yourself in Ghanaian Culture',
+      excerpt: 'Discover authentic ways to connect with the heart of Ghana...',
+      content: 'Full blog post content goes here...',
+      author: 'Alicia V',
+      date: 'May 15, 2025',
+      category: 'Cultural Travel',
+      imageUrl: '/images/ghana.jpg',
+      tags: ['Ghana', 'Culture', 'Africa'],
+    },
   ];
 
   return (
     <div>
-      <h1>Blog Posts</h1>
+      <h1>Our Blog</h1>
       {blogPosts.map((post) => (
         <div key={post.id}>
           <h2>{post.title}</h2>
-          <p><em>by {post.author} on {post.date}</em></p>
           <p>{post.excerpt}</p>
-          <Link to={`/blog/${post.id}`}>Read more</Link>
+          <Link to={`/blog/${post.id}`}>Read More</Link>
         </div>
       ))}
     </div>
